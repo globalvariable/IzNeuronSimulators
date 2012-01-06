dfd4bae761c14eef060461a30f4cc8eb6c532f20
Mehmet Kocaturk <kocaturk@gmail.com>
2012-01-06 16:47:17 +0200

    Added virtual neuron type of Parker-Sochacki integration paper.

--- Library/IzNeuron/NeuronTypes.c
+++ Library/IzNeuron/NeuronTypes.c
@@ -3,7 +3,25 @@
 
 void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b, double *c, double *d, double *k, double *C, double *v_resting, double *v_threshold, double *v_peak, bool *inhibitory, double *E_excitatory, double *E_inhibitory, double *tau_excitatory, double *tau_inhibitory)
 {
-	if (neuron_type == REGULAR_SPIKING) 
+	if (neuron_type == NRN_TYPE_VIRTUAL)
+	{
+		*v = -65;
+		*a = 0.03;
+		*b = -9.5;
+		*c = -85;
+		*d = 0;
+		*C = 200;
+		*k = 1.3;
+		*v_resting = -65;
+		*v_threshold = -50;
+		*v_peak = 48;
+		*inhibitory = 0;
+		*E_excitatory = -65;
+		*E_inhibitory = -65;
+		*tau_excitatory = 5;
+		*tau_inhibitory = 10;	
+	} 
+	else if (neuron_type == NRN_TYPE_REGULAR_SPIKING) 
 	{
 		*v = -60;
 		*a = 0.03;
@@ -21,7 +39,7 @@ void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b
 		*tau_excitatory = 5;
 		*tau_inhibitory = 10;
 	}
-	else if (neuron_type == INTRINSICALLY_BURSTING)  
+	else if (neuron_type == NRN_TYPE_INTRINSICALLY_BURSTING)  
 	{
 		*v = -75;	
 		*a =0.01;
@@ -39,7 +57,7 @@ void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b
 		*tau_excitatory = 5;
 		*tau_inhibitory = 10;				
 	}
-	else if (neuron_type == CHATTERING) 
+	else if (neuron_type == NRN_TYPE_CHATTERING) 
 	{
 		*v = -60;
 		*a =0.03;
@@ -57,7 +75,7 @@ void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b
 		*tau_excitatory = 5;
 		*tau_inhibitory = 10;		
 	}
-	else if (neuron_type == LOW_THRESHOLD) 
+	else if (neuron_type == NRN_TYPE_LOW_THRESHOLD) 
 	{
 		*v = -75;
 		*a =0.03;
@@ -75,7 +93,7 @@ void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b
 		*tau_excitatory = 5;
 		*tau_inhibitory = 10;		
 	}
-	else if (neuron_type == FAST_SPIKING)  
+	else if (neuron_type == NRN_TYPE_FAST_SPIKING)  
 	{
 		*v = -60;
 		*a =0.03;
@@ -98,23 +116,27 @@ void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b
 
 char *get_neuron_type_string(int neuron_type, char *str)
 {
-	if (neuron_type == REGULAR_SPIKING) 
+	if (neuron_type == NRN_TYPE_VIRTUAL) 
+	{
+		str = "Virtual Neuron";
+	}
+	else if (neuron_type == NRN_TYPE_REGULAR_SPIKING) 
 	{
 		str = "Regular Spiking";
 	}
-	else if (neuron_type == INTRINSICALLY_BURSTING)  
+	else if (neuron_type == NRN_TYPE_INTRINSICALLY_BURSTING)  
 	{
 		str = "Intrinsically Bursting";			
 	}
-	else if (neuron_type == CHATTERING) 
+	else if (neuron_type == NRN_TYPE_CHATTERING) 
 	{
 		str = "Chattering";		
 	}
-	else if (neuron_type == LOW_THRESHOLD) 
+	else if (neuron_type == NRN_TYPE_LOW_THRESHOLD) 
 	{
 		str = "(Inhibitory) Low Threshold";	
 	}
-	else if (neuron_type == FAST_SPIKING)  
+	else if (neuron_type == NRN_TYPE_FAST_SPIKING)  
 	{
 		str = "(Inhibitory) Fast Spikinig";		
 	}
--- Library/IzNeuron/NeuronTypes.h
+++ Library/IzNeuron/NeuronTypes.h
@@ -4,13 +4,15 @@
 #include <stdbool.h>
 
 // Neuron Types
-#define 	REGULAR_SPIKING 		0
-#define 	INTRINSICALLY_BURSTING	1
-#define 	CHATTERING				2
-#define 	LOW_THRESHOLD			3
-#define 	FAST_SPIKING			4
+#define 	NRN_TYPE_VIRTUAL 				0
+#define 	NRN_TYPE_REGULAR_SPIKING 		1
+#define 	NRN_TYPE_INTRINSICALLY_BURSTING	2
+#define 	NRN_TYPE_CHATTERING			3
+#define 	NRN_TYPE_LOW_THRESHOLD		4
+#define 	NRN_TYPE_FAST_SPIKING			5
 
-#define MAX_NUM_OF_NEURON_TYPES  5
+
+#define MAX_NUM_OF_NEURON_TYPES  6
 
 void get_neuron_type_parameters(int neuron_type, double *v, double *a, double *b, double *c, double *d, double *k, double *C, double *v_resting, double *v_threshold, double *v_peak, bool *inhibitory, double *E_excitatory, double *E_inhibitory, double *tau_excitatory, double *tau_inhibitory);
 
