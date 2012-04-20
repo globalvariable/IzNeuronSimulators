cc0f21174d92305be6cea9f7ddf6ce4d449f1813
Mehmet Kocaturk <kocaturk@gmail.com>
2012-04-21 02:46:58 +0300

    minor bug fix.

--- Library/MotorOutput/MotorOutputTypes.c
+++ Library/MotorOutput/MotorOutputTypes.c
@@ -4,11 +4,14 @@ bool get_motor_output_type_string(MotorOutputType motor_output_type, char *str)
 {
 	switch (motor_output_type)
 	{
+		case MOTOR_OUTPUT_NO_MOVE:
+			if (str != NULL)
+				strcpy(str, "MOTOR_OUTPUT_NO_MOVE");
+			return TRUE;
 		case MOTOR_OUTPUT_MOVE_TO_LEFT:
 			if (str != NULL)
 				strcpy(str, "MOTOR_OUTPUT_MOVE_TO_LEFT");
 			return TRUE;
-
 		case MOTOR_OUTPUT_MOVE_TO_RIGHT:
 			if (str != NULL)
 				strcpy(str, "MOTOR_OUTPUT_MOVE_TO_RIGHT");
